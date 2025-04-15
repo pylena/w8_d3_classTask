fa6144c25e12666a72ce1fdc15adcc33d56100ae
Lina Alsehli <141145520+pylena@users.noreply.github.com>
2025-04-15 20:24:58 +0300

    Delete In-Memory Caching directory

--- In-Memory Caching/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace In_Memory_Caching.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
